0f0819009b1f03c9f4630ef1e0d49a2972ebc121
mohammadiani <yoosef.m.1374@gmail.com>
2022-03-26 15:38:23 +0430

    first commit

---
+++ trust.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: trust pilot
+Plugin URI: http://mohammadiani.com
+Author: یوسف محمدیانی
+Version: 1.0.0
+Text Domain: trust pilot
+Domain Path: /languages
+Author URI: http://mohammadiani.com
+*/
